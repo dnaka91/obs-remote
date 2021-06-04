9c1bb5568e50037d837ed21f316915575e791ffc
Dominik Nakamura <dnaka91@gmail.com>
2021-06-04 01:36:43 +0000

    Replace vagrant with dev containers

---
+++ .devcontainer/bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -x
+
+HOME=/home/vscode
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+cat > $HOME/.cargo/config.toml <<-EOF
+    [target.x86_64-unknown-linux-gnu]
+    rustflags = ["-C", "link-arg=-fuse-ld=lld"]
+EOF
+
+rustup install nightly
+rustup component add llvm-tools-preview
+rustup component add llvm-tools-preview --toolchain nightly
+cargo install cargo-binutils cargo-edit grcov
+
+echo "export PATH=$HOME/.cargo/bin:$PATH" >> $HOME/.bashrc
+mkdir -p $HOME/.config/fish
+echo "set -Up fish_user_paths $HOME/.cargo/bin" >> $HOME/.config/fish/config.fish

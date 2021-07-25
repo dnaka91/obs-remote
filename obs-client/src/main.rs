8dc50449083e867c6061b0aab3522519c16c952c
Dominik Nakamura <dnaka91@gmail.com>
2021-07-26 01:24:13 +0900

    Add basic testing client

---
+++ obs-client/src/main.rs
@@ -0,0 +1,31 @@
+use anyhow::Result;
+
+use crate::{
+    general::general_client::GeneralClient,
+    v5::profiles::profiles_client::ProfilesClient as ProfilesClientV5,
+};
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let mut client = GeneralClient::connect("http://[::1]:50051").await?;
+    let version = client.get_version(()).await?.into_inner();
+
+    println!("{:#?}", version);
+
+    let mut client = ProfilesClientV5::connect("http://[::1]:50052").await?;
+    let version = client.list(()).await?.into_inner();
+
+    println!("{:#?}", version);
+
+    Ok(())
+}
+
+pub mod general {
+    tonic::include_proto!("obs_remote.general");
+}
+
+pub mod v5 {
+    pub mod profiles {
+        tonic::include_proto!("obs_remote.v5.profiles");
+    }
+}

9bed483674bb4f19619bf2f9cd77f6fea10b5062
Dominik Nakamura <dnaka91@gmail.com>
2021-08-11 10:59:26 +0900

    Implement most of the basic events for v4

--- obs-remote/src/obs_remote/events.rs
+++ obs-remote/src/obs_remote/events.rs
@@ -2,11 +2,15 @@
 
 use std::sync::Arc;
 
-use obs::{frontend::events, signal::SignalHandler};
+use obs::{
+    frontend::events::{self, Event as ObsEvent},
+    signal::SignalHandler,
+};
 use tokio::sync::{broadcast, mpsc, watch, Mutex};
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 
+use self::event_reply::Event;
 use self::events_server::Events;
 
 tonic::include_proto!("obs_remote.events");
@@ -23,24 +27,133 @@ impl Service {
 
         let event_handle = {
             let tx = tx.clone();
+            #[allow(clippy::match_same_arms)]
             events::add_callback(move |event| match event {
-                events::Event::SceneCollectionChanged => {
-                    let name = obs::frontend::scene_collections::current();
-
-                    tx.send(event_reply::Event::SceneCollectionChanged(
-                        SceneCollectionChanged { name },
-                    ))
+                ObsEvent::StreamingStarting => {
+                    tx.send(Event::StreamStarting(StreamStarting {})).ok();
+                }
+                ObsEvent::StreamingStarted => {
+                    tx.send(Event::StreamStarted(StreamStarted {})).ok();
+                }
+                ObsEvent::StreamingStopping => {
+                    tx.send(Event::StreamStopping(StreamStopping {})).ok();
+                }
+                ObsEvent::StreamingStopped => {
+                    tx.send(Event::StreamStopped(StreamStopped {})).ok();
+                }
+                ObsEvent::RecordingStarting => {
+                    tx.send(Event::RecordingStarting(RecordingStarting {})).ok();
+                }
+                ObsEvent::RecordingStarted => {
+                    tx.send(Event::RecordingStarted(RecordingStarted {
+                        filename: recording_filename(),
+                    }))
                     .ok();
                 }
-                events::Event::SceneCollectionListChanged => {
+                ObsEvent::RecordingStopping => {
+                    tx.send(Event::RecordingStopping(RecordingStopping {
+                        filename: recording_filename(),
+                    }))
+                    .ok();
+                }
+                ObsEvent::RecordingStopped => {
+                    tx.send(Event::RecordingStopped(RecordingStopped {
+                        filename: recording_filename(),
+                    }))
+                    .ok();
+                }
+                ObsEvent::SceneChanged => {}
+                ObsEvent::SceneListChanged => {}
+                ObsEvent::TransitionChanged => {
+                    let name = obs::frontend::transitions::current().name();
+
+                    tx.send(Event::SwitchTransition(SwitchTransition { name }))
+                        .ok();
+                }
+                ObsEvent::TransitionStopped => {}
+                ObsEvent::TransitionListChanged => {
+                    let transitions = obs::frontend::transitions::list()
+                        .into_iter()
+                        .map(|transition| transition.name())
+                        .collect();
+
+                    tx.send(Event::TransitionListChanged(TransitionListChanged {
+                        transitions,
+                    }))
+                    .ok();
+                }
+                ObsEvent::SceneCollectionChanged => {
+                    let name = obs::frontend::scene_collections::current();
+
+                    tx.send(Event::SceneCollectionChanged(SceneCollectionChanged {
+                        name,
+                    }))
+                    .ok();
+                }
+                ObsEvent::SceneCollectionListChanged => {
                     let collections = obs::frontend::scene_collections::list();
 
-                    tx.send(event_reply::Event::SceneCollectionListChanged(
+                    tx.send(Event::SceneCollectionListChanged(
                         SceneCollectionListChanged { collections },
                     ))
                     .ok();
                 }
-                _ => {}
+                ObsEvent::ProfileChanged => {
+                    let name = obs::frontend::profiles::current();
+
+                    tx.send(Event::ProfileChanged(ProfileChanged { name })).ok();
+                }
+                ObsEvent::ProfileListChanged => {
+                    let profiles = obs::frontend::profiles::list();
+
+                    tx.send(Event::ProfileListChanged(ProfileListChanged { profiles }))
+                        .ok();
+                }
+                ObsEvent::Exit => {
+                    tx.send(Event::Exiting(Exiting {})).ok();
+                }
+                ObsEvent::ReplayBufferStarting => {
+                    tx.send(Event::ReplayStarting(ReplayStarting {})).ok();
+                }
+                ObsEvent::ReplayBufferStarted => {
+                    tx.send(Event::ReplayStarted(ReplayStarted {})).ok();
+                }
+                ObsEvent::ReplayBufferStopping => {
+                    tx.send(Event::ReplayStopping(ReplayStopping {})).ok();
+                }
+                ObsEvent::ReplayBufferStopped => {
+                    tx.send(Event::ReplayStopped(ReplayStopped {})).ok();
+                }
+                ObsEvent::StudioModeEnabled => {
+                    tx.send(Event::StudioModeSwitched(StudioModeSwitched {
+                        enabled: true,
+                    }))
+                    .ok();
+                }
+                ObsEvent::StudioModeDisabled => {
+                    tx.send(Event::StudioModeSwitched(StudioModeSwitched {
+                        enabled: false,
+                    }))
+                    .ok();
+                }
+                ObsEvent::PreviewSceneChanged => {}
+                ObsEvent::SceneCollectionCleanup => {}
+                ObsEvent::FinishedLoading => {}
+                ObsEvent::RecordingPaused => {
+                    tx.send(Event::RecordingPaused(RecordingPaused {})).ok();
+                }
+                ObsEvent::RecordingUnpaused => {
+                    tx.send(Event::RecordingResumed(RecordingResumed {})).ok();
+                }
+                ObsEvent::TransitionDurationChanged => {}
+                ObsEvent::ReplayBufferSaved => {}
+                ObsEvent::VirtualcamStarted => {
+                    tx.send(Event::VirtualCamStarted(VirtualCamStarted {})).ok();
+                }
+                ObsEvent::VirtualcamStopped => {
+                    tx.send(Event::VirtualCamStopped(VirtualCamStopped {})).ok();
+                }
+                ObsEvent::TBarValueChanged => {}
             })
         };
 
@@ -48,7 +161,7 @@ impl Service {
             let tx = tx.clone();
             handler.connect("source_create", move |data| {
                 if let Some(source) = data.get_source() {
-                    tx.send(event_reply::Event::SourceCreated(SourceCreated {
+                    tx.send(Event::SourceCreated(SourceCreated {
                         name: source.name(),
                         ty: format!("{:?}", source.ty()),
                         kind: source.id(),
@@ -63,7 +176,7 @@ impl Service {
             let tx = tx.clone();
             handler.connect("source_destroy", move |data| {
                 if let Some(source) = data.get_source() {
-                    tx.send(event_reply::Event::SourceDestroyed(SourceDestroyed {
+                    tx.send(Event::SourceDestroyed(SourceDestroyed {
                         name: source.name(),
                         ty: format!("{:?}", source.ty()),
                         kind: source.id(),
@@ -124,4 +237,15 @@ impl Events for Service {
             Err(Status::aborted("server shutting down"))
         }
     }
+}
+
+fn recording_filename() -> String {
+    let output = obs::frontend::recording::output();
+    let settings = output.settings();
+
+    settings
+        .item_by_name("url")
+        .or_else(|| settings.item_by_name("path"))
+        .and_then(|item| item.string())
+        .unwrap_or_default()
 }
